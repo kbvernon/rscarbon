85a56341e7dfaaf1a202633597d0aa52902b5f2a
kbvernon <kenneth.b.vernon@gmail.com>
2024-06-02 16:22:54 -0600

    prepare calibration curve datasets

---
+++ data-raw/get-calibration-curves.R
@@ -0,0 +1,30 @@
+## code to prepare 14c calibration curve datasets
+
+get_14c <- function(x){
+
+  readr::read_csv(
+    file.path("https://intcal.org/", "curves", x),
+    skip = 11,
+    col_select = 1:3,
+    col_names = c("calbp", "c14bp", "tau"),
+    col_types = "i"
+  )
+
+}
+
+intcal13 <- get_14c("intcal13.14c")
+intcal20 <- get_14c("intcal20.14c")
+marine13 <- get_14c("marine13.14c")
+marine20 <- get_14c("marine20.14c")
+shcal13  <- get_14c("shcal13.14c")
+shcal20  <- get_14c("shcal20.14c")
+
+usethis::use_data(
+  intcal13,
+  intcal20,
+  marine13,
+  marine20,
+  shcal13,
+  shcal20,
+  overwrite = TRUE
+)
